ca8ef0a0f17d5f8b1dc5cde1cb06aeb7f112b193
Cabr1to <luis.b.c.melo@gmail.com>
2024-05-23 15:07:46 -0300

    listar_conteudos com endereco

--- pythonProject/search.py
+++ pythonProject/search.py
@@ -1,12 +1,10 @@
 import utils
 
-# Definição dos diretórios
 dir1 = "backup_Apple/S_9AE48BF4-AC26-473B-9C73-1EA9CB5F4D2A"
 dir2 = "backup_Apple/S_C676EA9A-DB38-4063-808C-3DA50C3AF90E"
 
 num = int(input("Qual diretório você deseja abrir? 1 ou 2.\n"))
 
-# Usa uma estrutura match-case para selecionar o diretório
 match num:
     case 1:
         print("Abrindo o diretório 1...")
@@ -15,5 +13,6 @@ match num:
         print("Abrindo o diretório 2...")
         utils.listar_conteudo(dir2)
     case _:
-        print("Opção inválida. Saindo do programa.")
-        exit(1)
+        print("Opção inválida, abrindo os 2 diretorios...")
+        utils.listar_conteudo(dir1)
+        utils.listar_conteudo(dir2)
--- pythonProject/utils.py
+++ pythonProject/utils.py
@@ -2,18 +2,16 @@ import os
 
 
 def listar_conteudo(diretorio):
-    # Verifica se o diretório existe
     if not os.path.exists(diretorio):
         print(f"O diretório {diretorio} não existe.")
         return
 
-    # Lista o conteúdo do diretório
     conteudo = os.listdir(diretorio)
     for elemento in conteudo:
         caminho_completo = os.path.join(diretorio, elemento)
         if os.path.isfile(caminho_completo):
             print("Arquivo:", elemento)
-            print("Caminho Completo:", caminho_completo)
+            print("Caminho:", caminho_completo)
             print("\n")
         elif os.path.isdir(caminho_completo):
             print("Diretório:", elemento)
