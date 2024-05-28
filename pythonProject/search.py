f49c519de2d12969c37a554d09d542e21218d455
Cabr1to <luis.b.c.melo@gmail.com>
2024-05-28 17:04:11 -0300

    test.json

--- pythonProject/search.py
+++ pythonProject/search.py
@@ -1,18 +1,24 @@
-import utils
+from pythonProject.utils import listar_conteudo, carregar_mapeamento
+
+mapping_file_path = "FileInfoList.txt"
 
 dir1 = "backup_Apple/S_9AE48BF4-AC26-473B-9C73-1EA9CB5F4D2A"
 dir2 = "backup_Apple/S_C676EA9A-DB38-4063-808C-3DA50C3AF90E"
+caminho_json = 'test.json'
 
 num = int(input("Qual diretório você deseja abrir? 1 ou 2.\n"))
+
 
 match num:
     case 1:
         print("Abrindo o diretório 1...")
-        utils.listar_conteudo(dir1)
+        listar_conteudo(dir1)
+
     case 2:
         print("Abrindo o diretório 2...")
-        utils.listar_conteudo(dir2)
+        listar_conteudo(dir2)
+        mapeamento = carregar_mapeamento(caminho_json)
     case _:
         print("Opção inválida, abrindo os 2 diretorios...")
-        utils.listar_conteudo(dir1)
-        utils.listar_conteudo(dir2)
+        listar_conteudo(dir1)
+        listar_conteudo(dir2)
--- pythonProject/utils.py
+++ pythonProject/utils.py
@@ -1,4 +1,7 @@
 import os
+import json
+
+mapping_file_path = "backup_Apple/FileInfoList.txt"
 
 
 def listar_conteudo(diretorio):
@@ -14,6 +17,22 @@ def listar_conteudo(diretorio):
             print("Caminho:", caminho_completo)
             print("\n")
         elif os.path.isdir(caminho_completo):
-            print("Diretório:", elemento)
+            print("Arquivo:", elemento)
             print("Caminho Completo:", caminho_completo)
             print("\n")
+
+
+def carregar_mapeamento(caminho_arquivo):
+    with open(caminho_arquivo, 'r', encoding='utf-8') as f:
+        f = open("test.json")
+        mapeamento_l = json.load(f)
+        xml = ""
+        for a in mapeamento_l:
+            print(a["id"])
+            for b in a:
+                if b == "fields":
+                    for c in a[b]:
+                        if c == "encryptedAttributes":
+                            xml = a[b][c]
+                            print(xml)
+    return mapeamento_l
